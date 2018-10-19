4aa3e087bdd5692f5b8994ff0fbcd2302f1782d8
Ngan Nguyen <ngancamnguyen@gmail.com>
2018-10-19 09:17:58 -0700

    add form and result div to html

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,17 @@
 $(document).ready(function(){
+  $("form#form-track").sumbit(function(event){
 
-alert('hello');
+  });
+
+
+// alert('hello');
+  event.preventDefault();
 });
+
+
+// Available Tracks
+// Ruby/Rails
+// PHP/Drupal
+// Java/Android
+// CSS/Design
+// C#/.NET
