4a9bf1c96e69418948736195f706bdea28adbabc
Ngan Nguyen <ngancamnguyen@gmail.com>
2018-10-19 14:14:06 -0700

    updated questions for survey

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,16 @@
-// working test code
-// $(document).ready(function() {
-//   $("#formOne").submit(function(event) {
-//     var nameInput = $("input#name").val();
-//
-//     $(".name").append(nameInput);
-//
-//     $("#story").show();
-//     event.preventDefault();
-//   });
-// });
-
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
     var nameInput = $("input#name").val();
-    var challangeInput = $("#challenge").val();
-    // var cultureInput = $("#culture").val();
-    // var projectInput = $("#projects").val();
-    // var locationInput = $("#location").val();
+    var challengeInput = $("#challenge").val();
+    var cultureInput = $("#culture").val();
+    var projectsInput = $("#projects").val();
+    var locationInput = $("#location").val();
 
     $(".name").append(nameInput);
     $(".challenge").append(challengeInput);
-    // $(".culture").append(cultureInput);
-    // $(".projects").append(projectsInput);
-    // $(".location").append(locationInput);
+    $(".culture").append(cultureInput);
+    $(".projects").append(projectsInput);
+    $(".location").append(locationInput);
 
     $("#story").show();
     console.log(challengeInput)
