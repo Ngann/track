6627f0c82391667a3cad67c6cf92fd2a80ae1b40
Ngan Nguyen <ngancamnguyen@gmail.com>
2018-10-19 13:51:08 -0700

    add var for questions in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,36 @@
+// working test code
+// $(document).ready(function() {
+//   $("#formOne").submit(function(event) {
+//     var nameInput = $("input#name").val();
+//
+//     $(".name").append(nameInput);
+//
+//     $("#story").show();
+//     event.preventDefault();
+//   });
+// });
+
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
     var nameInput = $("input#name").val();
+    var challangeInput = $("#challenge").val();
+    // var cultureInput = $("#culture").val();
+    // var projectInput = $("#projects").val();
+    // var locationInput = $("#location").val();
 
     $(".name").append(nameInput);
+    $(".challenge").append(challengeInput);
+    // $(".culture").append(cultureInput);
+    // $(".projects").append(projectsInput);
+    // $(".location").append(locationInput);
 
     $("#story").show();
+    console.log(challengeInput)
     event.preventDefault();
   });
 });
+
+
 
 // Available Tracks
 // Ruby/Rails
